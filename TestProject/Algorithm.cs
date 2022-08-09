aba045606757404b3a42335797c71c9070d63789
Riki-lu <r0527618125@gmail.com>
2022-08-09 03:06:05 +0300

    Step1_FindCustomerData

---
+++ TestProject/Algorithm.cs
@@ -0,0 +1,98 @@
+﻿using Kusto.Language;
+using Kusto.Language.Syntax;
+namespace TestProject
+{
+    public class Algorithm
+    {
+        //0
+        //wrapper function-call to the next steps{
+
+        //1 call Validation
+        //2 call PassQueryFindCustomerData
+        //call replace
+
+        //}
+
+        //1
+        /// <summary>
+        /// Validation checks to the query
+        /// </summary>
+        /// <param name="query">a Kusto query</param>
+        /// <returns>true if the query is correct and false if not</returns>
+        private static bool Validation(string query)
+        {
+            if (query == null)
+                return false;
+            var diagnostics = KustoCode.ParseAndAnalyze(query).GetDiagnostics();
+            return diagnostics.Count == 0;
+        }
+
+        //2
+        /// <summary>
+        /// pass the query, find the sensitve code.
+        /// </summary>
+        /// <param name="query">a Kusto query</param>
+        /// <param name="CustomerDataWords">list of all customer data had found</param>
+        /// <param name="code">Convert the query in order to prase it</param>
+        /// <returns>list of all customer data had found</returns>
+        // now it's public in order to run it but it will be private
+        public static List<string> PassQueryFindCustomerData(string query)
+        {
+            List<string> CustomerDataWords = new List<string>();
+            var code = KustoCode.Parse(query);
+            SyntaxElement.WalkNodes(code.Syntax,
+                n =>
+                {
+                    {
+                        switch (n.Kind)
+                        {
+                            //Sensitive Operators-might contain Customer Data
+                            //each Node operator represents root of tree, the first Descendant is the Customer Data word.
+
+                            case SyntaxKind.ExtendOperator:
+                                CustomerDataWords.Add(n.GetFirstDescendant<NameDeclaration>().ToString());
+                                break;
+                            case SyntaxKind.LetStatement:
+                                CustomerDataWords.Add(n.GetFirstDescendant<NameDeclaration>().ToString());
+                                break;
+                            case SyntaxKind.LookupOperator:
+                                CustomerDataWords.Add(n.GetFirstDescendant<NameDeclaration>().ToString());
+                                break;
+                            case SyntaxKind.SummarizeOperator:
+                                CustomerDataWords.Add(n.GetFirstDescendant<NameDeclaration>().ToString());
+                                break;
+                            case SyntaxKind.ProjectRenameOperator:
+                                CustomerDataWords.Add(n.GetFirstDescendant<NameDeclaration>().ToString());
+                                break;
+                            case SyntaxKind.AsOperator:
+                                CustomerDataWords.Add(n.GetFirstDescendant<NameDeclaration>().ToString());
+                                break;
+
+
+                            //Sensitive Parmeters-themselvs Customer Data word.
+
+                            case SyntaxKind.NamedParameter:
+                                CustomerDataWords.Add(n.ToString());
+                                break;
+                            case SyntaxKind.StringLiteralExpression:
+                                CustomerDataWords.Add(n.ToString());
+                                break;
+                            case SyntaxKind.FunctionCallExpression:
+                                //check if a function declaration called a customer data
+                                CustomerDataWords.Add(n.ToString());
+                                break;
+                            case SyntaxKind.SkippedTokens:
+                                //to check 
+                                break;
+                        }
+                    }
+                });
+            return CustomerDataWords;
+        }
+        //3
+        //Replace the customer data words 
+
+        //4
+        //return the clean query
+    }
+}
---
+++ TestProject/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static TestProject.Algorithm;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Example to run
+        string demoQuery = "Connections| where DestinationEntityType =~'AadIdentity'| extend a = a + b |let x= where a > 10.0";
+        var cleanQuery = PassQueryFindCustomerData(demoQuery);
+        Console.WriteLine("Source query: "+demoQuery);
+        Console.WriteLine("New query: " + cleanQuery);
+    }
+}
